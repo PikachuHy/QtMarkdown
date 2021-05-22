fd8deeb22d5715a9255936ab1853feb1bafafaed
PikachuHy <pikachuhy@163.com>
2021-05-22 16:22:04 +0800

    build: 将test.md放入qrc中

--- EditorMain.cpp
+++ EditorMain.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
     QApplication a(argc, argv);
     Editor w;
-    w.loadFile("test.md");
+    w.loadFile(":/test.md");
     w.resize(800, 600);
     w.show();
     return QApplication::exec();
